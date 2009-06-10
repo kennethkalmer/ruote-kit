2c783c070d917a15e02a5b1588345087e6d2eec3
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-06-11 00:58:41 +0200

    We have cucumber

---
+++ config/environments/cucumber.rb
@@ -0,0 +1,2 @@
+gem "cucumber", ">=0.3.11"
+gem "rspec", ">=1.2.6"
---
+++ features/support/env.rb
@@ -0,0 +1,10 @@
+# Sets up the DaemonKit environment for Cucumber
+ENV["DAEMON_ENV"] ||= "cucumber"
+require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
+require 'daemon_kit/cucumber/world'
+
+# Comment out the next line if you don't want Cucumber Unicode support
+require 'cucumber/formatter/unicode'
+
+DaemonKit::Application.running!
+require 'ruote-kit'
