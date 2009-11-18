b3f426b213db928ec05d970a458081475554c65f
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-11-18 18:43:07 +0200

    Basic /expressions API in place

--- lib/ruote-kit/resources/expressions.rb
+++ lib/ruote-kit/resources/expressions.rb
@@ -6,4 +6,50 @@ class RuoteKit::Application
       format.json { json( :status, :ok ) }
     end
   end
+
+  get "/expressions/:wfid" do
+    @process = engine.process( params[:wfid] )
+
+    if @process
+      respond_to do |format|
+        format.html { haml :expressions }
+        format.json { json( :expressions, {} ) }
+      end
+    else
+      resource_not_found
+    end
+  end
+
+  get "/expressions/:wfid/:expid" do
+    @process = engine.process( params[:wfid] )
+
+    if @process && @expression = @process.expressions.detect { |exp| exp.fei.expid == params[:expid] }
+      respond_to do |format|
+        format.html { haml :expression }
+        format.json { json( :expression, @expression ) }
+      end
+    else
+      resource_not_found
+    end
+  end
+
+  delete "/expressions/:wfid/:expid" do
+    process = engine.process( params[:wfid] )
+
+    if process && expression = process.expressions.detect { |exp| exp.fei.expid == params[:expid] }
+      if params[:_kill]
+        engine.kill_expression( expression.fei )
+      else
+        engine.cancel_expression( expression.fei )
+      end
+
+      respond_to do |format|
+        format.html { redirect "/expressions/#{params[:wfid]}" }
+        format.json { json( :status, :ok ) }
+      end
+    else
+      resource_not_found
+    end
+  end
+
 end
--- spec/resources/expressions_spec.rb
+++ spec/resources/expressions_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe "/expressions" do
+describe "GET /expressions" do
   it "should report a friendly message to the user (HTML)" do
     get "/expressions"
 
@@ -13,5 +13,163 @@ describe "/expressions" do
     last_response.should be_ok
 
     last_response.json_body['status'].should == 'ok'
+  end
+end
+
+describe "GET /expressions/wfid" do
+  describe "with running processes" do
+    before(:each) do
+      @wfid = launch_test_process
+    end
+
+    it "should render the expression tree (HTML)" do
+      get "/expressions/#{@wfid}"
+
+      last_response.should be_ok
+    end
+
+    it "should render the expression tree (JSON)" do
+      get "/expressions/#{@wfid}.json"
+
+      last_response.should be_ok
+    end
+  end
+
+  describe "without running processes" do
+    it "should 404 correctly (HTML)" do
+      get "/expressions/foo"
+
+      last_response.should_not be_ok
+      last_response.status.should be(404)
+    end
+
+    it "should 404 correctly (JSON)" do
+      get "/expressions/foo.json"
+
+      last_response.should_not be_ok
+      last_response.status.should be(404)
+    end
+  end
+end
+
+describe "GET /expressions/wfid/expid" do
+  describe "with running processes" do
+    before(:each) do
+      @wfid = launch_test_process
+      process = RuoteKit.engine.process( @wfid )
+      @nada_exp_id = process.expressions.last.fei.expid
+    end
+
+    it "should render the expression details (HTML)" do
+      get "/expressions/#{@wfid}/#{@nada_exp_id}"
+
+      last_response.should be_ok
+    end
+
+    it "should render the expression details (JSON)" do
+      get "/expressions/#{@wfid}/#{@nada_exp_id}.json"
+
+      last_response.should be_ok
+    end
+  end
+
+  describe "without running processes" do
+    it "should 404 correctly (HTML)" do
+      get "/workitems/foo/bar"
+
+      last_response.should_not be_ok
+      last_response.status.should be(404)
+    end
+
+    it "should 404 correctly (JSON)" do
+      get "/workitems/foo/bar.json"
+
+      last_response.should_not be_ok
+      last_response.status.should be(404)
+    end
+  end
+end
+
+describe "DELETE /expressions/wfid/expid" do
+  describe "with running processes" do
+    before(:each) do
+      @wfid = launch_test_process do
+        Ruote.process_definition :name => 'delete' do
+          sequence do
+            wait '1d', :on_cancel => 'bail_out'
+            echo 'done'
+          end
+
+          define 'bail_out' do
+            sequence do
+              echo 'bailed'
+            end
+          end
+        end
+      end
+
+      wait_exp = RuoteKit.engine.process( @wfid ).expressions.last
+      @expid = wait_exp.fei.expid
+    end
+
+    it "should cancel the expressions (HTML)" do
+      delete "/expressions/#{@wfid}/#{@expid}"
+
+      last_response.should be_redirect
+      last_response['Location'].should == "/expressions/#{@wfid}"
+
+      sleep 0.4
+
+      @tracer.to_s.should == "bailed\ndone"
+    end
+
+    it "should cancel the expressions (JSON)" do
+      delete "/expressions/#{@wfid}/#{@expid}.json"
+
+      last_response.should be_ok
+      last_response.json_body['status'].should == 'ok'
+
+      sleep 0.4
+
+      @tracer.to_s.should == "bailed\ndone"
+    end
+
+    it "should kill the expression (HTML)" do
+      delete "/expressions/#{@wfid}/#{@expid}?_kill=1"
+
+      last_response.should be_redirect
+      last_response['Location'].should == "/expressions/#{@wfid}"
+
+      sleep 0.4
+
+      @tracer.to_s.should == "done"
+    end
+
+    it "should kill the expression (JSON)" do
+      delete "/expressions/#{@wfid}/#{@expid}.json?_kill=1"
+
+      last_response.should be_ok
+      last_response.json_body['status'].should == 'ok'
+
+      sleep 0.4
+
+      @tracer.to_s.should == "done"
+    end
+  end
+
+  describe "without running processes" do
+    it "should 404 correctly (HTML)" do
+      delete "/expressions/foo/bar"
+
+      last_response.should_not be_ok
+      last_response.status.should be(404)
+    end
+
+    it "should 404 correctly (JSON)" do
+      delete "/expressions/foo/bar.json"
+
+      last_response.should_not be_ok
+      last_response.status.should be(404)
+    end
   end
 end
