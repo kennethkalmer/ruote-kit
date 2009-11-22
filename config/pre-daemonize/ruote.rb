564c78521d494c955dbdca0c191022c315aa8353
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-11-22 10:32:27 +0200

    Doc tlc (closes #1)

--- config/pre-daemonize/ruote.rb
+++ config/pre-daemonize/ruote.rb
@@ -2,14 +2,26 @@
 if File.directory?( DaemonKit.root + '/vendor/ruote' )
   $:.unshift( DaemonKit.root + '/vendor/ruote/lib' )
 else
-  puts "Expecting vendor/ruote to exist"
+  puts <<EOF
+
+Expecting vendor/ruote to exist!
+
+ruote-kit expects a copy of the ruote master branch
+(git://github.com/jmettraux/ruote) to exist in the
+#{DaemonKit.root}/vendor/ruote directory. You can safely
+symlink the code from somewhere else on your system, or
+clone the directory directly into vendor/.
+
+ruote-kit is now terminating.
+
+EOF
   exit 1
 end
 
 require 'ruote/engine'
 
-if !defined?( Ruote) #|| Ruote::VERSION < "2.0.0"
+if !defined?( Ruote )
   puts "Expecting ruote 2.0.0 or later"
   puts "Need to exit here..."
-  #exit 1
+  exit 1
 end
