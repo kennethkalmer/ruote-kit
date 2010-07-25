b3d454b752259630f4665890bd5005afb2dc17b2
John Mettraux <jmettraux@gmail.com>
2010-07-25 19:05:43 +0900

    trying to go minimal

--- lib/ruote-kit/helpers/navigation_helpers.rb
+++ lib/ruote-kit/helpers/navigation_helpers.rb
@@ -1,26 +1,17 @@
+
 module RuoteKit
   module Helpers
     module NavigationHelpers
 
-      def navigate_to( text, path )
-        css_classes = []
-        css_classes << 'first' if @first.nil?
-        @first = true
-
-        css_classes << 'active' if path.split('/')[2] == request.path.split('/')[2]
-
-        "<li class=\"#{css_classes.join(' ')}\"><a href=\"#{ path }\">#{text}</a></li>"
-      end
-
       def pluralize( number, word )
-        if number > 1
-          word << 's'
-        end
 
-        return [ number, word ].join(' ')
+        word << 's' if number > 1
+
+        return [ number, word ].join( ' ' )
       end
 
       def link_to( object, *args )
+
         case object
         when Ruote::ProcessStatus
           link_to_process( object )
@@ -32,18 +23,21 @@ module RuoteKit
       end
 
       def link_to_workitem( workitem )
+
         path = "/_ruote/workitems/#{workitem.fei.wfid}/#{workitem.fei.expid}"
 
         "<a href=\"#{path}\">GET #{path}</a>"
       end
 
       def link_to_process( status )
+
         path = "/_ruote/processes/#{status.wfid}"
 
         "<a href=\"#{path}\">GET #{path}</a>"
       end
 
       def link_to_expression( expression )
+
         path = "/_ruote/expressions/#{expression.fei.wfid}/#{expression.fei.expid}"
 
         "<a href=\"#{path}\">GET #{path}</a>"
