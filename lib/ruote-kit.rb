8c1651cfce3b696284c09ae40839fb05d890faa8
John Mettraux <jmettraux@gmail.com>
2010-10-11 23:16:32 +0900

    RuoteKit.run_worker(storage, join=true)
    auto-join can be disabled

--- lib/ruote-kit.rb
+++ lib/ruote-kit.rb
@@ -21,16 +21,26 @@ module RuoteKit
       @env ||= defined?(Rails) ? Rails.env : ENV['RACK_ENV'] || 'development'
     end
 
+    # Returns the storage participant associated automatically with any storage
+    #
     def storage_participant
       engine.storage_participant
     end
 
-    def run_worker (storage)
+    # Given a storage, runs a worker and sets RuoteKit.engine accordingly.
+    #
+    # By default, this method won't return (it will 'join' the worker). If you
+    # need to go on after this call, pass false as second parameter
+    # (especially useful in an EventMachine setting).
+    #
+    def run_worker(storage, join=true)
       RuoteKit.engine = Ruote::Engine.new(
-        Ruote::Worker.new(storage), :join => true)
+        Ruote::Worker.new(storage), :join => join)
     end
 
-    def bind_engine (storage)
+    # Uses the given storage for the RuoteKit.engine (no worker running here).
+    #
+    def bind_engine(storage)
       RuoteKit.engine = Ruote::Engine.new(storage)
     end
   end
