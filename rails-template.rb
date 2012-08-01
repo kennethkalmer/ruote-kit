019d0e7872c67fdc6996488e2aa6b64a03c87b9b
Torsten Schönebaum <torsten.schoenebaum@web.de>
2012-08-01 22:33:21 +0200

    Update Rails template to use git repos.
    Closes gh-21.

--- rails-template.rb
+++ rails-template.rb
@@ -1,6 +1,6 @@
 
-gem 'ruote', '>=2.2.0'
-gem 'ruote-kit', '>=2.2.0'
+gem 'ruote', :git => 'https://github.com/jmettraux/ruote.git'
+gem 'ruote-kit', :git => 'https://github.com/tosch/ruote-kit.git'
 
 rakefile 'ruote.rake', <<-CODE
   namespace :ruote do
